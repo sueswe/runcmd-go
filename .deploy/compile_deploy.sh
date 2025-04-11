42d3aeddc4777ff4d6fc849db5c49be2baf779f2
Werner Süß <werner.suess@itsv.at>
2025-04-11 12:13:12 +0200

    BUGFIX: .deploy/compile_deploy.sh

--- .deploy/compile_deploy.sh
+++ .deploy/compile_deploy.sh
@@ -18,21 +18,21 @@ echo "------------------------------------"
 
 echo ""
 echo "compiling: go build runcmd.go"
-go build runcmd.go || {
+go build runcmd.go /tmp/runcmd || {
     echo "Status: $?"
     exit 4
 }
 
 echo ""
 echo "compiling: GOOS=aix GOARCH=ppc64 go build -o runcmd.aix"
-GOOS=aix GOARCH=ppc64 go build -o runcmd.aix || {
+GOOS=aix GOARCH=ppc64 go build -o /tmp/runcmd.aix || {
     echo "Status: $?"
     exit 4
 }
 
 echo ""
 echo "compiling: GOOS=windows GOARCH=amd64 go build -o runcmd.win64"
-GOOS=windows GOARCH=amd64 go build -o runcmd.win64 || {
+GOOS=windows GOARCH=amd64 go build -o /tmp/runcmd.win64 || {
     echo "Status: $?"
     exit 4
 }
@@ -41,6 +41,7 @@ GOOS=windows GOARCH=amd64 go build -o runcmd.win64 || {
 
 # nun entsprechende scp's durchführen:
 echo "running viceversa.sh stp,testta3,14T4 runcmd \$HOME/bin"
+cd /tmp/
 viceversa.sh stp,testta3,14T4 runcmd \$HOME/bin || {
     echo "Status: $?"
     exit 4
