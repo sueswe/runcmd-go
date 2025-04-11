7ae847626f882665473dfdc64f30b87fe85d8149
Werner Süß <werner.suess@itsv.at>
2025-04-11 12:49:17 +0200

    BUGFIX: accessing AppReleaseVersion

--- runcmd.go
+++ runcmd.go
@@ -105,7 +105,7 @@ func run_with_p(command string, p string) (string, int) {
 }
 
 func main() {
-	release := os.Getenv("APPRELEASEVERSION")
+	AppReleaseVersion := os.Getenv("AppReleaseVersion")
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	//warningLog := log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
 	//errorLog := log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime)
