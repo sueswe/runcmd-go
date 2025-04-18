519418dcc6e5867e6a32510101e14347423930b7
Werner Süß <werner.suess@itsv.at>
2025-04-18 07:57:01 +0200

    new script for deployment

--- .deploy/compile.sh
+++ .deploy/compile.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-
 source ~/.profile
-
 
 cd "$HOME"/compile/runcmd-go || {
     echo "Status: $?"
     exit 4
 }
 
-
-
 echo "------------------------------------"
 env | grep PATH
 env | grep LOADED
+pwd
 echo "------------------------------------"
-
 
 APPRELEASEVERSION=$(git rev-list -1 HEAD runcmd.go)
 export APPRELEASEVERSION
@@ -29,25 +25,15 @@ go build -ldflags "-X main.REV=$APPRELEASEVERSION" -v -o /tmp/runcmd || {
 }
 
 echo ""
-echo "compiling: go build runcmd.go -ldflags -X main.REV=$APPRELEASEVERSION"
+echo "compiling: GOOS=aix GOARCH=ppc64 go build runcmd.go -ldflags -X main.REV=$APPRELEASEVERSION"
 GOOS=aix GOARCH=ppc64 go build -ldflags "-X main.REV=$APPRELEASEVERSION" -v -o /tmp/runcmd.aix || {
     echo "Status: $?"
     exit 4
 }
 
 echo ""
-echo "compiling: go build runcmd.go -ldflags -X main.REV=$APPRELEASEVERSION"
+echo "compiling: GOOS=windows GOARCH=amd64 go build runcmd.go -ldflags -X main.REV=$APPRELEASEVERSION"
 GOOS=windows GOARCH=amd64 go build -ldflags "-X main.REV=$APPRELEASEVERSION" -v -o /tmp/runcmd.win64 || {
-    echo "Status: $?"
-    exit 4
-}
-
-
-
-# nun entsprechende scp's durchführen:
-echo "running viceversa.sh stp,testta3,14T4 runcmd \$HOME/bin"
-cd /tmp/ || exit 1
-"$HOME"/bin/vicecersa.sh stp,testta3,14T4 runcmd \$HOME/bin || {
     echo "Status: $?"
     exit 4
 }
---
+++ .deploy/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source ~/.profile
+
+cd "$HOME"/compile/runcmd-go || {
+    echo "Status: $?"
+    exit 4
+}
+
+echo "------------------------------------"
+env | grep PATH
+env | grep LOADED
+echo "------------------------------------"
+
+
+
+cd /tmp/ || exit 1
+"$HOME"/bin/vicecersa.sh stp,testta3,14T4 runcmd \$HOME/bin || {
+    echo "Status: $?"
+    exit 2
+}
