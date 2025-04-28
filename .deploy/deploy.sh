bd5eb9e1c1496e4baad0222e7ec23c20fa4f2d0c
Werner Süß <werner.suess@itsv.at>
2025-04-28 08:04:46 +0200

    just build

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -14,15 +14,15 @@ echo "------------------------------------"
 
 
 
-cd /tmp/ || exit 1
-"$HOME"/bin/vicecersa.sh stp,testta3,14T4 runcmd \$HOME/bin || {
-    echo "Status: $?"
-    exit 2
-}
+# cd /tmp/ || exit 1
+# "$HOME"/bin/vicecersa.sh stp,testta3,14T4 runcmd \$HOME/bin || {
+#     echo "Status: $?"
+#     exit 2
+# }
 
-cd /tmp/ || exit 1
-"$HOME"/bin/vicecersa.sh lgkk,testta3,19Pt runcmd \$HOME/bin || {
-    echo "Status: $?"
-    exit 2
-}
+# cd /tmp/ || exit 1
+# "$HOME"/bin/vicecersa.sh lgkk,testta3,19Pt runcmd \$HOME/bin || {
+#     echo "Status: $?"
+#     exit 2
+# }
 
